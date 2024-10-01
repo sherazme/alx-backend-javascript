f20a399420adb019aaa0fd75941699bfcaa1905f
sherazme <your_github_email>
2024-10-01 02:24:03 +0000

    backend

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+xport default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
+}
